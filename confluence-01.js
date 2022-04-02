c680416dbb6d351208fc73e6c202b8d07243f402
MDN <mark.denardo@gmail.com>
2022-04-02 10:47:05 -0400

    Update confluence-01.js

--- confluence-01.js
+++ confluence-01.js
@@ -1,8 +1,8 @@
 //confluence by MDN
-
 const t=(()=>time)
 const sh=(s,m,k,o)=>(shape(s).rotate(t,1).kaleid(k).kaleid(k).kaleid(k).scale(0.5,0.5).modulate(m,0.1).out(o))
 const flt=(i,o)=>(src(i).repeat(t,1).mult(voronoi(t,t)).kaleid(3).out(o))
+//const flt1=(i,m,o)=>(src(i).modulate(m).scale(pi).out(o))
 
 sh(3,o1,3,o0)
 flt(o0,o1)
